d49982b0d92d6acc33b81ccb9ca4403a5caecdbe
dforkosh <danforkosh@yahoo.com>
2013-11-26 10:51:50 -0500

    dsgsg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reports::Application.config.secret_token = 'd7696f2401e50c4d556b65be18589a64c692d2f0b7c20e76288e3e418e6dc04ce5cf6f5597f0212124777bad9103e8edebd1761f284b3582ffe100944b72272c'
