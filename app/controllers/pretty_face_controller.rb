e6e07125ee494a81f1017088d9fecf031e79ae81
sheg <danforkosh@yahoo.com>
2013-12-09 16:52:35 -0500

    gdsgsdgsdgdsg

---
+++ app/controllers/pretty_face_controller.rb
@@ -0,0 +1,6 @@
+class PrettyFaceController < ApplicationController
+
+  def index
+    render file: File.join(Rails.root, 'public', 'index.html')
+  end
+end
---
+++ test/helpers/pretty_face_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PrettyFaceHelperTest < ActionView::TestCase
+end
